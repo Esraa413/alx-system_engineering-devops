1f0cc7b9b675e9833199b76d137e32c5f16f3930
Esraa413 <esraaabdelwahab614@gmail.com>
2023-12-02 14:49:14 -0800

    simple

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# regular expression must be exactly matching a string
+puts ARGV[0].scan(/hbt*n/).join
---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Using the project instructions
+puts ARGV[0].scan(/^h.n$/).join
