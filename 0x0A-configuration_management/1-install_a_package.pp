62d853810478d7c1642bb3f4a4d382a97fc7b145
Esraa413 <esraaabdelwahab614@gmail.com>
2024-01-19 15:35:56 -0800

    cm

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
+#!/usr/bin/pup
 # using install flask from pip3
 
 package { 'flask' :
 ensure   => '2.1.0',
+provider => 'pip3',
+}
+
+package { 'python_pip':
+ensure => installed,
+}
+
+package { 'werkzeug':
+ensure -> '2.0.1',
+provider => 'pip',
+require => Package['python3-pip'],
 }
