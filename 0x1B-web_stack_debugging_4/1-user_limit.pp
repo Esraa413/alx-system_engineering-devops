5f049dbcfa2b91075cc61da2bedd91d8ef4e3e3e
Esraa413 <esraaabdelwahab614@gmail.com>
2024-03-13 07:45:18 -0700

    deplogout

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# configuration so that it is possible to login with the holberton
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
